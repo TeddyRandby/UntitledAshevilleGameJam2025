0770f24740a3a831a3fba2fd1d930e1434626607
Teddy Randby <tedrandby@gmail.com>
2025-08-26 00:43:55 -0400

    Update entity rendering

--- data/entity/init.lua
+++ data/entity/init.lua
@@ -1,29 +1,30 @@
-
 local EntityTypes = require("data.entity.types")
 
 local M = {}
 
 for _, v in ipairs(EntityTypes) do
 	M[v.type] = v
+	if v.spritename then
+		v.sprite = love.graphics.newImage("resources/" .. v.spritename)
+	end
 end
 
 function M.create(type)
+  assert(M[type] ~= nil, "MISSIN GENTITY TYPE: " .. type)
+	local potential_entity = table.copy(M[type])
 
-  local potential_entity = table.copy(M[type])
+	if potential_entity.create then
+		potential_entity = potential_entity.create(potential_entity) or potential_entity
+	end
 
-  if potential_entity.create then
-    potential_entity.create(potential_entity)
-  end
-  
-  if not potential_entity.w then
-    potential_entity.w = 1
-  end
-  if not potential_entity.h then
-    potential_entity.h = 1
-  end
+	if not potential_entity.w then
+		potential_entity.w = 1
+	end
+	if not potential_entity.h then
+		potential_entity.h = 1
+	end
 
-
-  return potential_entity
+	return potential_entity
 end
 
 Entity = M
